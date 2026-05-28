1cb6086ff70011077d20304e58f1e8dcb7c69190
kajal swt <kajal@smart-webtech.in>
2026-05-28 13:36:39 +0530

    remove links

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -499,7 +499,7 @@
                                 document.write(new Date().getFullYear() + ",");
                             </script>
                             made with <i class="fa fa-heart"></i> by
-                            <a href="https://www.creative-tim.com" class="font-semibold text-slate-700 dark:text-white"
+                            <a href="#" class="font-semibold text-slate-700 dark:text-white"
                                 target="_blank">Creative Tim</a>
                             for a better web.
                         </div>
@@ -507,22 +507,22 @@
                     <div class="w-full max-w-full px-3 mt-0 shrink-0 lg:w-1/2 lg:flex-none">
                         <ul class="flex flex-wrap justify-center pl-0 mb-0 list-none lg:justify-end">
                             <li class="nav-item">
-                                <a href="https://www.creative-tim.com"
+                                <a href="#"
                                     class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-in-out text-slate-500"
                                     target="_blank">Creative Tim</a>
                             </li>
                             <li class="nav-item">
-                                <a href="https://www.creative-tim.com/presentation"
+                                <a href="#"
                                     class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-in-out text-slate-500"
                                     target="_blank">About Us</a>
                             </li>
                             <li class="nav-item">
-                                <a href="https://creative-tim.com/blog"
+                                <a href="#"
                                     class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-in-out text-slate-500"
                                     target="_blank">Blog</a>
                             </li>
                             <li class="nav-item">
-                                <a href="https://www.creative-tim.com/license"
+                                <a href="#"
                                     class="block px-4 pt-0 pb-1 pr-0 text-sm font-normal transition-colors ease-in-out text-slate-500"
                                     target="_blank">License</a>
                             </li>
